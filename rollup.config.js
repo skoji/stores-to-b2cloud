6cebf384b20f099ca3bc2abaa9649d30a025cf00
Satoshi Kojima <skoji@skoji.jp>
2020-04-25 19:42:02 +0900

    read SJIS csv

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,13 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+const pkg = require('./package.json');
+
 export default {
   input: 'src/main.js',
   output: {
     file: 'build/index.js',
     format: 'iife'
-  }
+  },
+  plugins: [resolve(), commonjs()]
 }
