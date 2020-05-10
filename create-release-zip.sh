d913800fd9ae98673c07063197102f577378d2a0
Satoshi Kojima <skoji@skoji.jp>
2020-05-10 12:58:21 +0900

    include default-data-template.json and deploy-template.sh in the release package

--- create-release-zip.sh
+++ create-release-zip.sh
@@ -2,6 +2,6 @@
 
 mkdir release-$1
 mkdir release-$1/build
-cp index.html release-$1/
+cp index.html default-data-template.json deploy-template.sh release-$1/
 cp build/index.js release-$1/build
 zip -r release-$1.zip release-$1/
